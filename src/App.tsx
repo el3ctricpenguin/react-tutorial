f86a767d1b41248b47be0653e0a4c0faf0e3a141
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-06-01 17:46:19 +0900

    add cat king switcher button using state

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
 interface MyButtonProps {
     title: string;
     disabled: boolean;
+    isCatKing: boolean;
+    setIsCatKing: Dispatch<SetStateAction<boolean>>;
 }
 
-function MyButton({ title, disabled }: MyButtonProps) {
-    return <button disabled={disabled}>{title}</button>;
+function CatStatus({ isCatKing }: { isCatKing: boolean }) {
+    return <>cat status: {String(isCatKing)}</>;
+}
+
+function MyButton({ title, disabled, isCatKing, setIsCatKing }: MyButtonProps) {
+    return (
+        <button disabled={disabled} onClick={() => setIsCatKing(!isCatKing)}>
+            {title}
+        </button>
+    );
 }
 
 export default function App() {
+    const [isCatKing, setIsCatKing] = useState(false);
     return (
         <>
+            <h2>cute cat status</h2>
+            <CatStatus isCatKing={isCatKing} />
             <h3>cute cat</h3>
-            <MyButton title={"cute cat button"} disabled={false} />
+            <MyButton title={"cute cat button"} disabled={false} isCatKing={isCatKing} setIsCatKing={setIsCatKing} />
             <h3>cute disabled cat</h3>
-            <MyButton title={"cute disabled cat button"} disabled={true} />
+            <MyButton title={"cute disabled cat button"} disabled={true} isCatKing={isCatKing} setIsCatKing={setIsCatKing} />
         </>
     );
 }
